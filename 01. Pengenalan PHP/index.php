7ad5f74ff8aa0662415c5195b6116cdb6331dc7e
Henry Zai <hjuniorzai@gmail.com>
2023-03-10 20:54:35 +0700

    pengenalan php

---
+++ 01. Pengenalan PHP/index.php
@@ -0,0 +1,26 @@
+<?php
+  echo "Fikom Unika";
+  echo "Elsa Sijabat";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pemrograman Web Lanjut</title>
+</head>
+<body>
+  <?php
+    echo "<h3>Fikom Unika</h3>";
+    echo "<marquee>Sistem Informasi</marquee>";
+
+    // membuat variabel
+    $a = 4;
+    $b = 10;
+    $c = $a + $b;
+    echo "$a + $b = $c";
+  ?>
+</body>
+</html>
